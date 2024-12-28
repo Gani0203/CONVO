3c3daebfd0472dbe8acf7f9c2464bca2756128e4
EMMADI GANESH <134721736+Gani0203@users.noreply.github.com>
2024-12-28 19:47:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtnIuyPHlSKqlIvcSSRnNfXbkNQxw4Gaw",
-  authDomain: "convo-74286.firebaseapp.com",
-  projectId: "convo-74286",
-  storageBucket: "convo-74286.firebasestorage.app",
-  messagingSenderId: "192765261480",
-  appId: "1:192765261480:web:5e85947f0c28c215f3749d",
-  measurementId: "G-NBE2V18CS3",
+ //use your firebase configuration
 };
 
 export const app = initializeApp(firebaseConfig);
